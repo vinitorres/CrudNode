85b2b74f339455496680df3f3f0e7bc7490ef7e1
Andre Vinicius Torres Conrado <vinicius.torresc@gmail.com>
2026-02-06 06:57:45 -0300

    Debug: log initialization

--- src/database/client.ts
+++ src/database/client.ts
@@ -2,6 +2,8 @@ import { PrismaClient } from '@prisma/client'
 import { Pool } from 'pg'
 import { PrismaPg } from '@prisma/adapter-pg'
 
+console.log('[Database] Initializing Prisma Client with Driver Adapter...')
+
 // Use POSTGRES_URL (Vercel Integration) or DATABASE_URL
 let connectionString = process.env.POSTGRES_URL || process.env.DATABASE_URL
 
