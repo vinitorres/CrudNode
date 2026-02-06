614e0d8f15a64e1593c87f3d61c21c7906fef341
Andre Vinicius Torres Conrado <vinicius.torresc@gmail.com>
2026-02-06 06:02:22 -0300

    feat: Add and configure `@fastify/cors` to enable Cross-Origin Resource Sharing for the application.

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,14 @@
 import 'dotenv/config'
 import routes from './routes'
 import Fastify from 'fastify'
+import cors from '@fastify/cors'
 
 export const app = Fastify({
     logger: true
+})
+
+app.register(cors, {
+    origin: true // Allows all origins
 })
 
 app.register(routes)
